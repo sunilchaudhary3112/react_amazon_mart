42cc170922712711ec26c79a1e8fcc22a4d705bf
sunilchaudhary3112 <sunilchaudhary3112@gmail.com>
2020-06-28 02:31:19 +0530

    added basic node/express server with babel support for es6

---
+++ backend/data.js
@@ -0,0 +1,35 @@
+export default  {
+    products: [
+        {
+            _id: '1',
+            name: 'Slim Shirt',
+            category: 'Shirts',
+            image: '/images/d1.jpg',
+            brand: 'Nike',
+            price: 60,
+            rating: 4.5,
+            numReviews: 10
+        },
+        {
+            _id: '2',
+            name: 'Fit Shirt',
+            category: 'Shirts',
+            image: '/images/d1.jpg',
+            brand: 'Nike',
+            price: 40,
+            rating: 4.2,
+            numReviews: 10
+        },
+        {
+            _id: '3',
+            name: 'Best Pants',
+            category: 'Pants',
+            image: '/images/d1.jpg',
+            brand: 'Nike',
+            price: 80,
+            rating: 4.1,
+            numReviews: 10
+        },
+
+    ]
+}
