4530b059cfb53604c11da44a111ce443bfb8ab91
sunilchaudhary3112 <sunilchaudhary3112@gmail.com>
2020-06-27 17:53:57 +0530

    added the basic ui to react to display static products array

---
+++ frontend/src/App.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import logo from './logo.svg';
+import './index.css';
+import data  from "./data";
+
+function App() {
+  const openMenu = () => {
+    document.querySelector(".sidebar").classList.add("open")
+  }
+
+  const closeMenu = () => {
+    document.querySelector(".sidebar").classList.remove("open")
+  }
+  return (
+    <div className="grid-container">
+      <header className="header">
+        <div className="brand">
+          <button onClick={openMenu}>
+            &#9776;
+            </button>
+          <a href="index.html">Amazon Mart</a>
+        </div>
+        <div className="header-links">
+          <a href="cart.html">Cart</a>
+          <a href="signin.html">Sign In</a>
+        </div>
+      </header>
+      <aside className="sidebar">
+        <h3>Shopping Categories</h3>
+        <button className="sidebar-close-button" onClick={closeMenu}>x</button>
+        <ul>
+          <li>
+            <a href="index.html">pants</a>
+          </li>
+          <li>
+            <a href="index.html">shirts</a>
+          </li>
+        </ul>
+      </aside>
+
+      <main className="main">
+        <div className="content">
+          <ul className="products">
+            {
+              data.products.map(product =>
+                <li className="product">
+                  <img className="product-image" src={product.image} alt="product"></img>
+                  <div className="product-name">
+                    <a href="product.html">{product.name}</a>
+                  </div>
+                  <div className="product-brand">{product.brand}</div>
+                  <div className="product-price">{product.price}</div>
+                  <div className="product-rating">{product.rating} Star {product.numReviews} Reviews</div>
+                </li>
+              )
+            }
+
+          </ul>
+        </div>
+      </main>
+
+      <footer className="footer">
+        All rights reserved
+            </footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/data.js
@@ -0,0 +1,32 @@
+export default  {
+    products: [
+        {
+            name: 'Slim Shirt',
+            category: 'Shirts',
+            image: '/images/d1.jpg',
+            brand: 'Nike',
+            price: 60,
+            rating: 4.5,
+            numReviews: 10
+        },
+        {
+            name: 'Fit Shirt',
+            category: 'Shirts',
+            image: '/images/d1.jpg',
+            brand: 'Nike',
+            price: 40,
+            rating: 4.2,
+            numReviews: 10
+        },
+        {
+            name: 'Best Pants',
+            category: 'Pants',
+            image: '/images/d1.jpg',
+            brand: 'Nike',
+            price: 80,
+            rating: 4.1,
+            numReviews: 10
+        },
+        
+    ]
+}
